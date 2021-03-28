c8413c26c169aea420982a28c86ea33780fb61d0
asussepol <asussepol@gmail.com>
2021-03-28 10:46:29 -0300

    primeiro commit

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,49 @@
+import { useState,useEffect } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
+
+
+
+import "../styles/repositories.scss";
+
+
+// https://api.github.com/orgs/rocketseat/repos
+
+// const repository = {                     // todas as informaçãoes de um depositorio em um unico objeto
+
+//     name:"unform2",
+//     descricao:"forms in React",
+//     link:"https://github.com/unform/unform"
+
+// }
+export function RepositoryList() {
+
+    const [repositorios,setRepositorios]= useState([])  // como é uma lista vazia se inicia a com o vetor vazio [] para iniciar o estado
+   
+   useEffect(()=>{
+       fetch('https://api.github.com/orgs/rocketseat/repos')
+       .then(response=>response.json())
+       .then(data=>setRepositorios(data))
+   },[])
+
+
+
+    return (
+
+
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+
+
+            <ul>
+                    {repositorios.map(
+                        repository=>{
+                            return <RepositoryItem key={repository.name} repository={repository} />
+                        }
+                    )}
+                
+                
+                
+            </ul>
+        </section>
+    );
+}
